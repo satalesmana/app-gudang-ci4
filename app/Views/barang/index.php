4f49f0e2451fb198e91bd4debdac7df4dfd20289
sata <lesmanasata@gmail.com>
2021-10-18 20:48:15 +0700

    form barang

--- app/Views/barang/index.php
+++ app/Views/barang/index.php
@@ -2,6 +2,7 @@
     <h1 class="h2">Master Data - Barang</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group me-2">
+        <button type="button" data-bs-toggle="modal" data-bs-target="#formBarangModal"  class="btn btn-sm btn-outline-secondary">Add Item</button>
         <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
         <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
         </div>
@@ -10,4 +11,43 @@
         This week
         </button>
     </div>
-    </div>
+</div>
+
+<table class="table table-bordered table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nama Barang</th>
+      <th scope="col">Qty</th>
+      <th scope="col">Harga Beli</th>
+      <th scope="col">Harga Jual</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Spidol</td>
+      <td>10</td>
+      <td>Rp. 5000</td>
+      <td>Rp. 6000</td>
+      <td>
+          <button class="btn btn-primary btn-sm">Edit</button>
+          <button class="btn btn-danger btn-sm">Delete</button>
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>Spidol</td>
+      <td>10</td>
+      <td>Rp. 5000</td>
+      <td>Rp. 6000</td>
+      <td>
+          <button class="btn btn-primary btn-sm">Edit</button>
+          <button class="btn btn-danger btn-sm">Delete</button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<?= $this->include('barang/form_barang'); ?>
